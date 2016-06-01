ad5a089f3a7af2d2dac14238c810d748d4480909
Jessica Dias <jeh_dias18@hotmail.com>
2016-06-01 18:45:03 -0300

    Hello World

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jessica
+ *
+ * Created on 1 de Junho de 2016, 18:41
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hello Word");
+
+    return (0);
+}
+
